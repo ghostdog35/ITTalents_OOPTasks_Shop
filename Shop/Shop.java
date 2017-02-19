fe9ca4da8bab7422e998a5403bb812db6c724800
Milen Gerganov <mgerganov@gmail.com>
2017-02-19 14:46:23 +0200

    fixed some repeating code

--- Shop/Shop.java
+++ Shop/Shop.java
@@ -15,32 +15,19 @@ public class Shop {
 		this.address = address;
 		this.money = money;
 		this.products= new ArrayList<>();
-		Product meso = new Product(productType.KG, "Meso", 8, 50);
-		Product sirene = new Product(productType.KG, "Sirene", 6, 50);
-		Product kashkaval = new Product(productType.KG, "Kashkaval", 10, 50);
-		Product salam = new Product(productType.KG, "Salam",15, 50);
-		Product lukanka = new Product(productType.KG, "Lukanka",20, 50);
-		Product bahur = new Product(productType.KG, "Bahur", 4, 50);
-		Product bira = new Product(productType.PIECE, "Bira", 1, 150);
-		Product vino = new Product(productType.PIECE, "Vino", 10, 150);
-		Product rakiq = new Product(productType.PIECE, "Rakiq", 8, 150);
-		Product vodka = new Product(productType.PIECE, "Vodka", 7, 150);
-		Product whiskey = new Product(productType.PIECE, "Whiskey", 25, 150);
-		Product voda = new Product(productType.PIECE, "Voda", 1, 150);
-		Product cola = new Product(productType.PIECE, "Coca Cola", 1, 150);
-		this.addProduct(meso);
-		this.addProduct(sirene);
-		this.addProduct(kashkaval);
-		this.addProduct(salam);
-		this.addProduct(lukanka);
-		this.addProduct(bahur);
-		this.addProduct(bira);
-		this.addProduct(vino);
-		this.addProduct(rakiq);
-		this.addProduct(vodka);
-		this.addProduct(whiskey);
-		this.addProduct(voda);
-		this.addProduct(cola);
+		this.addProduct(new Product(productType.KG, "Meso", 8, 50));
+		this.addProduct(new Product(productType.KG, "Sirene", 6, 50));
+		this.addProduct(new Product(productType.KG, "Kashkaval", 10, 50));
+		this.addProduct(new Product(productType.KG, "Salam",15, 50));
+		this.addProduct(new Product(productType.KG, "Lukanka",20, 50));
+		this.addProduct(new Product(productType.KG, "Bahur", 4, 50));
+		this.addProduct(new Product(productType.PIECE, "Bira", 1, 150));
+		this.addProduct(new Product(productType.PIECE, "Vino", 10, 150));
+		this.addProduct(new Product(productType.PIECE, "Rakiq", 8, 150));
+		this.addProduct(new Product(productType.PIECE, "Vodka", 7, 150));
+		this.addProduct(new Product(productType.PIECE, "Whiskey", 25, 150));
+		this.addProduct(new Product(productType.PIECE, "Voda", 1, 150));
+		this.addProduct(new Product(productType.PIECE, "Coca Cola", 1, 150));
 	}
 	
 	public Product getProduct(String name, int qty) {
